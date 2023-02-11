7abedf9e65700026c05e13879f6ed6b841622574
Gooi Hean Zhen <hz.gooi@outlook.com>
2023-02-12 00:13:24 +0800

    Update email_auth.dart

--- lib/email_auth.dart
+++ lib/email_auth.dart
@@ -18,7 +18,7 @@ late String _finalEmail;
 /// This function will check if the provided email ID is valid or not
 bool _isEmail(String email) {
   String p =
-      r"/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
   RegExp regExp = new RegExp(p);
   return regExp.hasMatch(email);
 }
